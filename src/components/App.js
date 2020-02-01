cd81b028196a07608fd23016ed9b03d1196d7162
Tebasaki314 <h.takemura480@gmail.com>
2020-02-01 13:10:54 +0900

    part5

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,33 @@ import React, { Component } from 'react';
 import TrelloList from "./TrelloList";
 import { connect } from "react-redux";
 import TrelloActionButton from "./TrelloActionButton";
+import { DragDropContext } from "react-beautiful-dnd";
 
 class App extends Component {
-  render() {
 
+  onDragEnd = () => {
+    // TODO: reordering logic
+  }
+
+  render() {
     const { lists } = this.props;
     return (
-      <div className="App">
-        <h2>Hello YouTube</h2>
-        <div style={styles.listsContainer}>
-          {lists.map(list => (
-            <TrelloList
-              listId={list.id}
-              key={list.id}
-              title={list.title}
-              cards={list.cards}
-            />
-          ))}
-          <TrelloActionButton list />
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div className="App">
+          <h2>Hello YouTube</h2>
+          <div style={styles.listsContainer}>
+            {lists.map(list => (
+              <TrelloList
+                listId={list.id}
+                key={list.id}
+                title={list.title}
+                cards={list.cards}
+              />
+            ))}
+            <TrelloActionButton list />
+          </div>
         </div>
-      </div>
+      </DragDropContext>
     );
   }
 }
--- src/reducers/listsReducer.js
+++ src/reducers/listsReducer.js
@@ -6,32 +6,32 @@ let cardId = 4;
 const initialState = [
     {
         title: "Last Episode",
-        id: 0,
+        id: `list-${0}`,
         cards: [
             {
-                id: 0,
+                id: `card-${0}`,
                 text: "we created a static list and a static card"
             },
             {
-                id: 1,
+                id: `card-${1}`,
                 text: "we used a mix between material Ui React and styled components"
             }
         ]
     },
     {
         title: "This Episode",
-        id: 1,
+        id: `list-${1}`,
         cards: [
             {
-                id: 0,
+                id: `card-${2}`,
                 text: "we will create our firlst reducer"
             },
             {
-                id: 1,
+                id: `card-${3}`,
                 text: "and render many cards on our list with static data"
             },
             {
-                id: 2,
+                id: `card-${4}`,
                 text: "we will also make some little changes I forgot in the last episode (link tags for roboto font and icons, ...)"
             }
         ]
@@ -43,7 +43,7 @@ const listsReducer = (state = initialState, action) => {
         case CONSTANTS.ADD_LIST:
             const newList = {
                 title: action.payload,
-                id: listId,
+                id: `list-${listId}`,
                 cards: []
             };
             listId += 1;
@@ -52,7 +52,7 @@ const listsReducer = (state = initialState, action) => {
         case CONSTANTS.ADD_CARD:
             const newCard = {
                 text: action.payload.text,
-                id: cardId
+                id: `card-${cardId}`
             }
             cardId += 1;
 
